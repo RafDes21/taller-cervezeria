29e1342f6cb61fd258d5f800947f9437d01108a6
unknown <israeltineo84@gmail.com>
2022-09-20 17:55:24 -0300

    agregue footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,40 @@
 import React from "react";
 import "./footer.css";
-import { AiOutlineInstagram, AiOutlineFacebook } from "react-icons/ai";
+import {
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="container footer">
       <div className="row">
-        <div className="col-4 footerDerechos  d-flex">
+        <div className="col-4 footerDerechos ">
+          <h6>Unite al equipo!</h6>
           <p>Todos los derechos reservados</p>
         </div>
         <div className="col-4 footerLogo">
-          <img src="./logos/logo-carousel.png" alt="logoCarrusel"/>
-          <h6>Unite al equipo!</h6>
+          <img className="footer-logo" src="./logos/logo.png" alt="logo" />
+          <p className="footer-p">
+            +54-22152312 Lunes a domingos de 9 a 23 hs calle 1 nro 2345, pasaje
+            ferreira
+          </p>
         </div>
-        <div className="col-4 footerRedes d-flex">
-          <a href="!#">
-            <AiOutlineInstagram />
-          </a>
-          <a href="!#">
-            <AiOutlineFacebook />
-          </a>
+        <div className="col-4 footerRedes">
+          <h4>Seguinos:</h4>
+          <div className="caja-footerRedes">
+            <Link className="redes" to="/lugar">
+              <AiOutlineInstagram />
+            </Link>
+            <Link className="redes" to="/lugar">
+              <AiOutlineFacebook />
+            </Link>
+            <Link className="redes" to="/lugar">
+              <AiOutlineMail />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
