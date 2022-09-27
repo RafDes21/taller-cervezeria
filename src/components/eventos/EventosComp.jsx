231a98be30249d8774d44954b9e58f569e4ea387
Juan Albertsen <85770676+juanalbertsen@users.noreply.github.com>
2022-09-27 20:58:28 -0300

    componente eventos

--- src/components/eventos/EventosComp.jsx
+++ src/components/eventos/EventosComp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header, Texto } from "../Header/Header";
+import "./eventosComp.css"
 
 const EventosComp = () => {
   const img = "./assets/eventos/eventos.png";
@@ -9,8 +10,43 @@ const EventosComp = () => {
     <div>
       <div>
         <Header img={img} />
+        <Texto texto1={h1} texto2={h3} />
       </div>
-      <Texto texto1={h1} texto2={h3} />
+      <section className="Eventos-content">
+        <p className="Eventos-text">
+      Nos ocupamos de organizar tu evento. Contanos los detalles del mismo y solicita una cotización. Un asesor se encargará de enviarte los servicios que ofrecemos y el valor de los mismos.
+        </p>
+
+        <div className="Eventos-form">
+          <form action="">
+            <div className="inputContainer">
+              <input type="text" id="nombre" className="" placeholder="Nombre y apellido"/>
+              <label for="nombre" class="label">Nombre</label>
+            </div>
+            <div className="inputContainer">
+              <input type="number" id="tel" className="" placeholder="Número de celular"/>
+              <label for="tel" class="label">Teléfono</label>
+            </div>
+            <div className="inputContainer">
+              <input type="mail" id="mail" className="" placeholder="Dirección de correo electrónico"/>
+              <label for="mail" class="label">E-mail</label>
+            </div>
+            <div className="inputContainer">
+              <input type="date" id="fecha" className="" placeholder="fecha"/>
+              <label for="fecha" class="label">Fecha</label>
+            </div>
+            <div className="inputContainer">
+              <input type="text" id="cant" className="" placeholder="Cantidad de invitados"/>
+              <label for="cant" class="label">Cantidad de personas</label>
+            </div>
+            <div className="inputContainer">
+              <input type="textarea" id="comentario"className="" placeholder="Déjanos tu comentario"/>
+              <label for="comentario" class="label"></label>
+            </div>
+          </form>
+        </div>
+
+      </section>
     </div>
   );
 };
--- src/components/menu/MenuCom.jsx
+++ src/components/menu/MenuCom.jsx
@@ -10,42 +10,37 @@ const MenuCom = () => {
 
   return (
     <div className="Menu-wrapper">
-      <Header img={img} />
-      <section className="Menu-content">
+        <Header img={img} />
         <Texto texto1={h1} texto2={h3} />
+      <section className="Menu-content">
         <div className="Menu-comida">
           <div className="Menu-comida-card">
-            <img src="#" alt="imagen menu" />
-            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-            <p>Molestias omnis, quos asperiores, consequatur accusamus </p>
+            <img src="./img/menu/menuA.png" alt="imagen menu" />
+            <h5>Alitas por kilo</h5>
+            <p>Dos sabores a elección</p>
           </div>
           <div className="Menu-comida-card">
-            <img src="#" alt="imagen menu" />
-            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-            <p>Molestias omnis, quos asperiores, consequatur accusamus </p>
+            <img src="./img/menu/menub.png" alt="imagen menu" />
+            <h5>Pozole</h5>
+            <p>Caldo a base de maiz con torillas</p>
           </div>
           <div className="Menu-comida-card">
-            <img src="#" alt="imagen menu" />
-            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-            <p>Molestias omnis, quos asperiores, consequatur accusamus </p>
+            <img src="./img/menu/menuc.png" alt="imagen menu" />
+            <h5>La monster taller </h5>
+            <p>Hamburguesa doble con piña y salsa bbq</p>
           </div>
           <div className="Menu-comida-card">
-            <img src="#" alt="imagen menu" />
-            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-            <p>Molestias omnis, quos asperiores, consequatur accusamus </p>
-          </div>
-          <div className="Menu-comida-card">
-            <img src="#" alt="imagen menu" />
-            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-            <p>Molestias omnis, quos asperiores, consequatur accusamus </p>
+            <img src="./img/menu/menud.png" alt="imagen menu" />
+            <h5>Tacos Baterías</h5>
+            <p>Bistec, cebolla y cilantro. </p>
           </div>
         </div>
         <div className="Menu-bebidas">
           <h4>Nuestras Bebidas</h4>
           <div className="Menu-bebidas-slider">
-            <img src="#" alt="img bebidas" />
-            <img src="#" alt="img bebidas" />
-            <img src="#" alt="img bebidas" />
+            <img src="./img/menu/bebidasA.png" alt="img bebidas" />
+            <img src="./img/menu/bebidasB.png" alt="img bebidas" />
+            <img src="./img/menu/bebidasC.png" alt="img bebidas" />
           </div>
         </div>
         <div className="Menu-carta">
