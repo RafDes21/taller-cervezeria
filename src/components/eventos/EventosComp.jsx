34886b5b74c443fe2c682fb63911f9c80414b3b2
Juan Albertsen <85770676+juanalbertsen@users.noreply.github.com>
2022-09-28 15:06:42 -0300

    update componente menu

--- src/components/eventos/EventosComp.jsx
+++ src/components/eventos/EventosComp.jsx
@@ -8,11 +8,11 @@ const EventosComp = () => {
   const h3 = "Diversión a toda marcha";
   return (
     <div>
-      <div>
+      <div className="wrapper mx-auto">
         <Header img={img} />
         <Texto texto1={h1} texto2={h3} />
       </div>
-      <section className="Eventos-content">
+      <section className="Eventos-content mx-auto">
         <p className="Eventos-text">
       Nos ocupamos de organizar tu evento. Contanos los detalles del mismo y solicita una cotización. Un asesor se encargará de enviarte los servicios que ofrecemos y el valor de los mismos.
         </p>
@@ -43,10 +43,42 @@ const EventosComp = () => {
               <input type="textarea" id="comentario"className="" placeholder="Déjanos tu comentario"/>
               <label for="comentario" class="label"></label>
             </div>
+            <div className="inputContainer Button-wrapper">
+              <a href=""className="Form-button">
+                <button >Cotizá</button>
+              </a>
+            </div>
           </form>
         </div>
+              </section>
 
-      </section>
+        <div className="Eventos-detalle">
+            <p>
+              Ven a disfrutar de nuestras noches únicas:
+            </p>
+
+            <div className="Eventos-detalle-content">
+            <img src="./assets/vector/vectoreventos.png" className="Eventos-detalle-vector" alt="" />  
+            <div className="Eventos-detalle-wrapper">
+              <div className="karaoke">
+                <img src="./img/eventos/eventosA.png" alt="karaoke en el taller" />
+                <>
+                <h4>Karaoke</h4>
+                <p>Viernes 7pm</p>
+                </>
+              </div>
+              <div className="bandas">
+                <>
+                <h4>Bandas en<br/>vivo</h4>
+                <p>Viernes 7pm</p>
+                </>
+                <img src="./img/eventos/eventosB.png" alt="bandas en el taller" />
+              </div>
+            </div>
+            </div>
+        </div>
+
+
     </div>
   );
 };
