bb5d5c5007a812d241741a31293dcd56a203ef74
Juan Albertsen <85770676+juanalbertsen@users.noreply.github.com>
2022-10-07 17:35:32 -0300

    update form

--- src/components/reservas/ReservasComp.jsx
+++ src/components/reservas/ReservasComp.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, Texto } from "../Header/Header";
 import "./ReservasComp.css";
+import {useForm} from "react-hook-form"
 
 const ReservasComp = () => {
   const img = "./assets/reservas/reservas.png";
   const h1 = "RESERVAS";
   const h3 = "Vive experiencias únicas";
+
+
+  const {register, handleSubmit, watch } = useForm()
+  const [msj, setMsj] = useState("")
+
+  const onSubmit = (data) => {
+    console.log(data);   
+    const name = watch("Nombre");
+    const phone= watch("Telefono");
+    const mail = watch("Mail");
+    const date = watch("Fecha");
+    const time = watch("Horario");
+
+  const msj = `¡Hola! Mi nombre es ${name} y me gustaría reservar una mesa a las ${time}hs del día ${date}. ¡Muchas Gracias! Mi mail de contacto es: ${mail}`;
+  window.open(`https://api.whatsapp.com/send?phone=+541136945314&text=${msj}`)
+      };
+
+
 
   return (
     <div className="">
@@ -19,12 +38,13 @@ const ReservasComp = () => {
         </p>
         <div className="Reservas-form">
           <h2 className="Reservas-h2">Reserva tu mesa</h2>
-          <form action="https://formsubmit.co/albertsenjuanm@gmail.com" method="POST">
+          <form onSubmit={handleSubmit(onSubmit)} >
             <div className="inputContainer">
               <input
                 type="text"
                 id="nombre"
                 name="nombre"
+                {...register('Nombre')}
                 className=""
                 placeholder="Nombre y apellido"
               />
@@ -37,6 +57,7 @@ const ReservasComp = () => {
                 type="number"
                 id="tel"
                 name="telefono"
+                {...register('Telefono')}
                 className=""
                 placeholder="Número de celular"
               />
@@ -49,6 +70,7 @@ const ReservasComp = () => {
                 type="mail"
                 id="mail"
                 name="mail"
+                {...register('Mail')}
                 className=""
                 placeholder="Dirección de correo electrónico"
               />
@@ -57,7 +79,8 @@ const ReservasComp = () => {
               </label>
             </div>
             <div className="inputContainer">
-              <input type="date" id="fecha" className="" name="fecha" placeholder="fecha" />
+              <input type="date" id="fecha" className="" name="fecha" placeholder="fecha"
+              {...register('Fecha')} />
               <label for="fecha" class="label">
                 Fecha
               </label>
@@ -70,6 +93,7 @@ const ReservasComp = () => {
                 type=""
                 id="time"
                 className=""
+                {...register('Horario')}
                 name="horario"
                 placeholder="Selecciona un horario"
               >
